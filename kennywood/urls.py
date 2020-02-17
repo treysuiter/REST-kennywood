3320ab176b836777d26d10e44b07402a318a00bc
Trey Suiter <trey.suiter@gmail.com>
2020-02-17 15:22:09 -0600

    models created

--- kennywood/urls.py
+++ kennywood/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
+from kennywoodapi.models import *
 
 router = routers.DefaultRouter(trailing_slash=False)
 
