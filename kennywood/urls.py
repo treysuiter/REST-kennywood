6f31e50673dd918f7fab6097884c7c3f218aae83
Trey Suiter <trey.suiter@gmail.com>
2020-02-18 09:40:46 -0600

    added auth thing

--- kennywood/urls.py
+++ kennywood/urls.py
@@ -3,12 +3,15 @@ from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 from kennywoodapi.models import *
 from kennywoodapi.views import ParkAreas
+from kennywoodapi.views import register_user, login_user
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'parkareas', ParkAreas, 'parkarea')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('register/', register_user),
+    path('login/', login_user),
     path('api-token-auth/', obtain_auth_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- kennywoodapi/views/__init__.py
+++ kennywoodapi/views/__init__.py
@@ -1,1 +1,3 @@
 from .parkareas import ParkAreas
+from .register import register_user
+from .register import login_user
